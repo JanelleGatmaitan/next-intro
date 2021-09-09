17065e903d5577405ddca38af1703ab62039341d
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-09-09 15:06:48 -0700

    initial commit

---
+++ pages/cars/[id].js
@@ -0,0 +1,42 @@
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+export default function Car({ car }) {
+
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <>
+    <h1>Car {id}</h1>
+      <div className="img-container">
+        <img className="img-sm" src={car.image} alt="car"/>
+      </div>
+    </>
+  )
+}
+
+export async function getStaticProps({ params }) {
+
+  const req = await fetch(`http://localhost:3000/${params.id}.json`);
+  const data = await req.json();
+
+  return {
+    props: { car: data },
+  }
+}
+
+export async function getStaticPaths() {
+
+  const req = await fetch('http://localhost:3000/cars.json');
+  const data = await req.json();
+
+  const paths = data.map(car => {
+    return { params: { id: car } }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
